a4c0e9399bffb89d4a66c4b2292e1d17a8034cfa
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-09-18 15:49:36 -0400

    made bug fix

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0; //original line: int s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
         return s;
+        // Analysis of change
+        // Key Fix: Unlike Python, there is a max value to which Java Ints can store. Since the final answer...
+        // should be 500_000_500_000L, this is way too much for an integer to store.
+        // Therefore, the accumulator variable representing the sum (s) should be marked as a LONG
     }
 }
